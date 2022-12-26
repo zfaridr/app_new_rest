7576ce292b38c8157775effbacc661d802c5579e
zfaridr <zainullovfr@gmail.com>
2022-12-26 13:23:03 +0300

    lesson_1_1

---
+++ app_new/views.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Author
+from .serializers import AuthorModelSerializer
+
+class AuthorModelViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorModelSerializer
