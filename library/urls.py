94e084df86673633b1f9d0b49581843ad0420411
zfaridr <95588065+zfaridr@users.noreply.github.com>
2023-01-16 09:02:15 +0300

    lesson_3 (#3)

--- library/urls.py
+++ library/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from app_new.views import UserModelViewSet
+from app_new.views import UserModelViewSet, ProjectModelViewSet, ToDoModelViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('todo', ToDoModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
