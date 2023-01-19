69df0b05170f7d1d81513099e1cdd35456f475b5
zfaridr <95588065+zfaridr@users.noreply.github.com>
2023-01-19 08:33:20 +0300

    lesson_4 (#4)

--- app_new/views.py
+++ app_new/views.py
@@ -1,15 +1,38 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import User, Project, ToDo
 from .serializers import UserModelSerializer, ProjectModelSerializer, ToDoModelSerializer
+from rest_framework.pagination import LimitOffsetPagination
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
 
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+      
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    
+    def get_queryset(self):
+        name = self.request.query_params.get('project_name')
+        projects = Project.objects.all()
+        if name:
+            project = Project.filter(name__contains=name)
+        return projects
 
 class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
+    pagination_class = ToDoLimitOffsetPagination
+    
+
+    
+
+
